530e02f61e83c4376e6f16e91ca878612baa6774
Tátila Bianca Durão Fernandes <91564423+tatidurao@users.noreply.github.com>
2025-06-02 14:10:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,6 +8,22 @@ video = cv2.VideoCapture("bb3.mp4")
 
 #lendo primeiro quadro
 
+while True:
+
+    check, img = video.read()
+    
+    cv2.imshow("resultado", img)
+
+
+    # Saia da janela de exibição quando a barra de espaço for pressionada
+    key = cv2.waitKey(25)
+    if key == 32:
+        print("Interrompido")
+        break
+
+video.release()
+cv2.destroyALLwindows()
+
 
 
 
